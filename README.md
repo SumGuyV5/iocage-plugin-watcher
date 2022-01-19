1b0a9662e1ddbbbc9da32aa9172c30d3a3568df9
Richard <richard2891@gmail.com>
2022-01-18 19:28:35 -0500

    inital commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# iocage-plugin-watcher
+
+Watcher is an automated movie NZB & Torrent searcher and snatcher. You can add a list of wanted movies and Watcher will automatically send the NZB or Torrent to your download client.
---
+++ post_install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -x
+IP_ADDRESS=$(ifconfig | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}')
+
+
+cd /usr/local/bin
+ln -s python3.7 python
+
+cd /usr/local/share && git clone https://github.com/nosmokingbandit/Watcher3.git
+
+mv Watcher3 watcher
+
+pw user add media -c media -u 710 -d /nonexistent -s /usr/bin/nologin
+chown -R media:media watcher
+
+cp /usr/local/share/watcher/run\ scripts/FreeBSD/watcher /usr/local/etc/rc.d/watcher
+chmod 555 /usr/local/etc/rc.d/watcher
+
+sysrc watcher_enable=YES
+sysrc watcher_data_dir="/usr/local/share/watcher"
+
+service watcher start
+
+echo -e "Watcher now installed.\n" > /root/PLUGIN_INFO
+echo -e "\nGo to $IP_ADDRESS:9090\n" >> /root/PLUGIN_INFO
