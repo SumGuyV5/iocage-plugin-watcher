8c740661ce423496db44b024c75c14da9c16ded8
Richard <richard2891@gmail.com>
2022-02-22 21:02:30 -0500

    fix whitespace. add http to PLUGIN_INFO output.

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -x
 IP_ADDRESS=$(ifconfig | grep -E 'inet.[0-9]' | grep -v '127.0.0.1' | awk '{ print $2}')
 
-
 cd /usr/local/bin
 ln -s python3.7 python
 
@@ -13,7 +12,7 @@ pw user add media -c media -u 710 -d /nonexistent -s /usr/bin/nologin
 chown -R media:media watcher
 
 cp /usr/local/share/watcher/run\ scripts/FreeBSD/watcher /usr/local/etc/rc.d/watcher
-chmod 555 /usr/local/etc/rc.d/watcher
+chmod 755 /usr/local/etc/rc.d/watcher
 
 sysrc watcher_enable=YES
 sysrc watcher_data_dir="/usr/local/share/watcher"
@@ -21,4 +20,4 @@ sysrc watcher_data_dir="/usr/local/share/watcher"
 service watcher start
 
 echo -e "Watcher now installed.\n" > /root/PLUGIN_INFO
-echo -e "\nGo to $IP_ADDRESS:9090\n" >> /root/PLUGIN_INFO
+echo -e "\nGo to http://$IP_ADDRESS:9090\n" >> /root/PLUGIN_INFO
